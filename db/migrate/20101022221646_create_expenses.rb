623aac1fb27685277f5f24e653720b1d58d7f355
Marcin Nowak <Marcin.Nowak@sonyx.net>
2010-10-23 00:17:36 +0200

    expenses scaffold added

---
+++ db/migrate/20101022221646_create_expenses.rb
@@ -0,0 +1,16 @@
+class CreateExpenses < ActiveRecord::Migration
+  def self.up
+    create_table :expenses do |t|
+      t.string :date
+      t.string :person
+      t.string :name
+      t.string :shareholders
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :expenses
+  end
+end
